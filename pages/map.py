a00f036888c3b8bc58a6d410783a6ae49c79c482
eyvonne <eyvonne@users.noreply.github.com>
2020-04-22 20:43:29 -0700

    adding updated map page

--- pages/map.py
+++ pages/map.py
@@ -11,6 +11,8 @@ import numpy as np
 import datetime
 from app import app
 
+SOURCES = ['USGS', 'EMSC']
+
 column1 = dbc.Col(
     [
         dcc.Markdown(
@@ -27,37 +29,41 @@ column1 = dbc.Col(
 
             """
         ),
-        html.Div(
-            [
-                dcc.Dropdown(
-                    id="source",
-                    options=[
-                        {"label": "USGS", "value": "USGS"},
-                        {"label": "EMSC", "value": "EMSC"},
-                    ],
-                    value="USGS",
-                ),
-                dcc.Dropdown(
-                    id="timeFrame",
-                    options=[
-                        {"label": "Last Quake", "value": "Quake"},
-                        {"label": "Last Hour", "value": "/hour"},
-                        {"label": "Last Day", "value": "/day"},
-                        {"label": "Last Week", "value": "/week"},
-                        {"label": "Last Month", "value": "/month"},
-                    ],
-                    value="Quake",
-                ),
-                html.Div(id="menuItems"),
-            ]
-        ),
-        html.Div(
-            [
-                dcc.Markdown("\n\n\n\n"),
-                dcc.Slider(id="magnitude", min=0, max=10, step=0.5, value=5.5),
-                dcc.Markdown(id="sliderOutput"),
-            ]
-        ),
+        html.Div([
+            dcc.Dropdown(
+                id='source',
+                options=[
+                    {'label': 'USGS', 'value': 'USGS'},
+                    {'label': 'EMSC', 'value': 'EMSC'},
+                    {'label': 'BOTH', 'value': 'BOTH'}
+                ],
+                value='USGS'
+            ),
+
+            dcc.Dropdown(
+                id='timeFrame',
+                options=[
+                    {'label': 'Last Quake', 'value': 'Quake'},
+                    {'label': 'Last Hour', 'value': '/hour'},
+                    {'label': 'Last Day', 'value': '/day'},
+                    {'label': 'Last Week', 'value': '/week'},
+                    {'label': 'Last Month', 'value': '/month'}
+                ],
+                value='Quake'
+            ),
+            html.Div(id='menuItems')
+        ]),
+        html.Div([
+            dcc.Markdown("\n\n\n\n"),
+            dcc.Slider(
+                id='magnitude',
+                min=0,
+                max=10,
+                step=.5,
+                value=5.5
+            ),
+            dcc.Markdown(id='sliderOutput')
+        ])
     ],
     md=2,
 )
@@ -66,88 +72,130 @@ column1 = dbc.Col(
 
 
 @app.callback(
-    dash.dependencies.Output("sliderOutput", "children"),
-    [dash.dependencies.Input("magnitude", "value")],
-)
+    dash.dependencies.Output('sliderOutput', 'children'),
+    [dash.dependencies.Input('magnitude', 'value')])
 def display_min_mag(mag_num):
     return f"Minimum Magnitude: {mag_num}"
 
 
 @app.callback(
-    dash.dependencies.Output("wheretheDataGoes", "children"),
-    [
-        dash.dependencies.Input("timeFrame", "value"),
-        dash.dependencies.Input("magnitude", "value"),
-        dash.dependencies.Input("source", "value"),
-    ],
-)
+    dash.dependencies.Output('wheretheDataGoes', 'children'),
+    [dash.dependencies.Input('timeFrame', 'value'),
+     dash.dependencies.Input('magnitude', 'value'),
+     dash.dependencies.Input('source', 'value')])
 def update_output(value, mag, source):
-    if value == "Quake":
-        api_url = (
-            f"https://quake-ds-staging.herokuapp.com/last{value}/{source}/{float(mag)}"
-        )
+    if source != 'BOTH':
+        print('running with one source')
+        return single_source(value, mag, source)
     else:
-        api_url = (
-            f"https://quake-ds-staging.herokuapp.com/last/{source}/{value}/{float(mag)}"
-        )
-    data = requests.get(api_url)
-    if (
-        type(data.json()["message"]) == type({1: "a"})
-        or type(data.json()["message"]) == type([1, 2, 3])
-        and len(data.json()["message"]) >= 1
-    ):
-        df = (
-            pd.DataFrame(data.json()["message"])
-            if value != "Quake"
-            else pd.DataFrame(data.json()["message"], index=[0])
-        )
+        print('running with two sources')
+        return dual_source(value, mag)
+
+
+def dual_source(value, mag):
+    if value == 'Quake':
+        return dual_last(mag)
+    else:
+        df = pd.DataFrame()
+        for source in SOURCES:
+            api_url = f'https://quake-ds-staging.herokuapp.com/last/{source}/{value}/{float(mag)}'
+            data = requests.get(api_url)
+            if data.json()['num_quakes'] != 0:
+                df = df.append(data.json()['message'])
+    if df.shape[0] > 0:
+        title = f'Quakes over {mag} from Both USGS and EMSC in the last {value.strip("/")}'
+        df['color'] = df['Oceanic'].apply(lambda x: 'yellow' if x != x else 'blue')
         data, layout = loaded_fig(df)
-        if value == "Quake":
-            title = f"Last Quake over {mag} in {source}"
+    else:
+        title = f'No Quakes over {mag} from either USGS or EMSC in the last {value.strip("/")}'
+        data, layout = empty_fig()
+
+    fig = go.Figure(data=data, layout=layout)
+    fig.update_layout(mapbox_style='stamen-terrain', height=700, title=title)
+    return dcc.Graph(figure=fig)
+
+
+def dual_last(mag):
+    quakes = []
+    for i, source in enumerate(SOURCES):
+        api_url = f'https://quake-ds-staging.herokuapp.com/lastQuake/{source}/{float(mag)}'
+        data = requests.get(api_url)
+        if data.json()['num_quakes'] > 0:
+            quakes.append(data.json()['message'])
+
+    if len(quakes) > 0:
+        title = f'Last Quake over {mag}'
+        data = quakes[0] if quakes[0]['time'] > quakes[1]['time'] else quakes[1]
+        df = pd.DataFrame(data, index=[0])
+        df['color'] = df['Oceanic'].apply(lambda x: 'yellow' if x != x else 'blue')
+        data, layout = loaded_fig(df)
+    else:
+        title = f'No Quakes Over {mag} in either USGS or EMSC'
+        data, layout = empty_fig()
+
+    fig = go.Figure(data=data, layout=layout)
+    fig.update_layout(mapbox_style='stamen-terrain', height=700, title=title)
+    return dcc.Graph(figure=fig)
+
+
+def single_source(value, mag, source):
+    if value == 'Quake':
+        api_url = f'https://quake-ds-staging.herokuapp.com/last{value}/{source}/{float(mag)}'
+    else:
+        api_url = f'https://quake-ds-staging.herokuapp.com/last/{source}/{value}/{float(mag)}'
+    data = requests.get(api_url)
+    if data.json()['num_quakes'] != 0:
+        df = pd.DataFrame(data.json()['message']) if value != 'Quake' else \
+            pd.DataFrame(data.json()['message'], index=[0])
+        df['color'] = 'blue' if source == 'USGS' else 'yellow'
+        data, layout = loaded_fig(df)
+        if value == 'Quake':
+            title = f'Last Quake over {mag} in {source}'
         else:
             title = f"Quakes over {mag} in the last {value.strip('last/')} in {source}"
 
     else:
         data, layout = empty_fig()
-        if value == "Quake":
-            title = f"No Quakes over {mag} to display in {source}"
+        if value == 'Quake':
+            title = f'No Quakes over {mag} to display in {source}'
         else:
             title = f"No Quakes over {mag} in the last {value.strip('last/')} to display in {source}"
 
     fig = go.Figure(data=data, layout=layout)
-    fig.update_layout(
-        mapbox_style="stamen-terrain",
-        height=600,
-        title=title,
-        margin={"r": 0, "t": 0, "l": 0, "b": 0},
-    )
-
+    fig.update_layout(mapbox_style='stamen-terrain', height=700, title=title)
     return dcc.Graph(figure=fig)
 
 
 def loaded_fig(df):
-    df["lat"] = df["lat"].apply(lambda x: str(x))
-    df["lon"] = df["lon"].apply(lambda x: str(x))
+    df['lat'] = df['lat'].apply(lambda x: str(x))
+    df['lon'] = df['lon'].apply(lambda x: str(x))
     data = [
         go.Scattermapbox(
-            lat=df["lat"],
-            lon=df["lon"],
-            mode="markers",
-            marker=go.scattermapbox.Marker(size=df["mag"] + 9),
-            # text=df[['place', 'time','mag']],
-            text=[
-                f"""place: {x['place']}<br>UTC time: {datetime.datetime.fromtimestamp(x['time']/1000.0)}<br>mag: {x['mag']}"""
-                for _, x in df.iterrows()
-            ],
-            hoverinfo="text",
+            lat=df['lat'],
+            lon=df['lon'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=df['mag'] + 9,
+                color=df['color'],
+            ),
+
+            text=[f"""place: {x['place']}<br>UTC time: {datetime.datetime.fromtimestamp(x['time']/1000.0)}<br>mag: {x['mag']}"""
+                  for _, x in df.iterrows()],
+            hoverinfo='text'
         )
     ]
 
     layout = go.Layout(
         autosize=True,
-        hovermode="closest",
+        hovermode='closest',
         mapbox=go.layout.Mapbox(
-            bearing=0, center=go.layout.mapbox.Center(lat=0, lon=0), pitch=0, zoom=0.5
+            bearing=0,
+            center=go.layout.mapbox.Center(
+                lat=0,
+                lon=0
+            ),
+            pitch=0,
+            zoom=.5
         ),
     )
     return data, layout
@@ -155,18 +203,31 @@ def loaded_fig(df):
 
 def empty_fig():
     data = [
-        go.Scattermapbox(lat=[0], lon=[0], mode="text", textposition="middle center")
+        go.Scattermapbox(
+            lat=[0],
+            lon=[0],
+            mode='text',
+            textposition='middle center'
+        )
     ]
 
     layout = go.Layout(
         autosize=True,
         mapbox=go.layout.Mapbox(
-            bearing=0, center=go.layout.mapbox.Center(lat=0, lon=0), pitch=0, zoom=0.5
+            bearing=0,
+            center=go.layout.mapbox.Center(
+                lat=0,
+                lon=0
+            ),
+            pitch=0,
+            zoom=.5
         ),
     )
     return data, layout
 
 
-column2 = dbc.Col([html.Div(id="wheretheDataGoes")])
+column2 = dbc.Col([html.Div(
+    id='wheretheDataGoes')
+])
 
-layout = dbc.Row([column1, column2], style={"margin-top": 100, "height": 1000})
+layout = dbc.Row([column1, column2], style={'margin-top': 100, 'height': 1000})
